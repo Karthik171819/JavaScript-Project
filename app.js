cf8a6d13ccf6cd581549c24205a63701e1fdbc8b
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-04 11:16:29 +0530

    user checking

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let botscore = 0;
 const choices = document.querySelectorAll(".choice");
 
 const genCompChoice = () => {
-    const options = ["rock", "paper", "scissors"];
+    const options = ["rock", "paper", "scissor"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
 }
@@ -12,6 +12,14 @@ const genCompChoice = () => {
 const drawGame = () =>{
     console.log("game was draw.")
 }
+
+const showWinner = (useWin) => {
+    if (userWin){
+        console.log("you win! ");   
+    } else {
+        console.log("you lost");
+    }
+};
 
 const playGame = (userChoice) => {
     console.log("user choice =", userChoice);
@@ -22,9 +30,22 @@ const playGame = (userChoice) => {
     if (userChoice === compChoice){
         //Draw Game
         drawGame();
+    } else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            //scissor, paper
+            userWin = compChoice === "paper" ? false : true ;
+        } else if (userChoice === "paper") {
+            //rock, scissor
+            userWin = compChoice === "scissor" ? false : true;
+        } else {
+            //rock, paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin);
     }
 
-}
+};
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
