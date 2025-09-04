8a85f6f604d5051253f03485fd9d53f3ff53f808
Karthik171819 <karthikkumar092004@gmail.com>
2025-09-04 10:57:34 +0530

    SCripting

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 let userscore = 0;
 let botscore = 0;
 
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice =", userChoice);
+    //Computer choice
+    const compChoice = genCompChoice();
+    console.log("Bot Choice =", compChoice);
+
+    if (userChoice === comCHoice){
+        //Draw Game
+        
+    }
+
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
